110a6ed50f3218ef7767b568e38f9b7eb1295769
Aygen Yucel <aygenyucel@outlook.com>
2022-10-06 16:52:00 +0300

    updated

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/*
-ASSIGNMENT RULES
-- All the answers must be written in JavaScript
-- You can ask for help, reach the Teaching Assistants if needed
-- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
-- You can test your code in a separate file or de-commenting the single exercises in this one.
-- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
-- The solution must be available for the tutors by the end of the day (5PM CET)
-*/
-
 const { ObjectFlags } = require("typescript");
 
 /* EXERCISE 1
